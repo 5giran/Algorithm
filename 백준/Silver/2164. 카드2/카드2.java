f0a4e72aae395ffe57924a108c1e83d2016ec1bf
Giran Oh <ohgiran@gmail.com>
2025-05-12 15:46:40 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 28944 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,26 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        LinkedList<Integer> cardSet = new LinkedList<>();
+        Deque<Integer> cardSet = new ArrayDeque<>();
 
         for (int i = 1; i <= N; i++) {
             cardSet.add(i);
         }
 
         while (cardSet.size() > 1) {
-            cardSet.removeFirst();
-            int next = 0;
-            next = cardSet.getFirst();
-            cardSet.addLast(next);
-            cardSet.removeFirst();
+            cardSet.pollFirst();
+            cardSet.addLast(cardSet.pollFirst());
         }
 
-        System.out.println(cardSet.getFirst());
+        System.out.println(cardSet.peek());
+
 
     }
 }
