851183800579035def0e39f2eb125034ad408220
Giran Oh <ohgiran@gmail.com>
2025-09-30 17:22:03 +0900

    [Gold V] Title: 적록색약, Time: 108 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -4,10 +4,9 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static int N;
+    static char[][] grid, colorBlindGrid;
     static boolean[][] visited;
-    static boolean[][] visitedDal;
-
-
     static int[][] directions = {
             {-1, 0},
             {1, 0},
@@ -15,7 +14,9 @@ public class Main {
             {0, 1}
     };
 
-    static void findArea(int row, int col, int N, char[][] area) {
+
+    // 같은 구역 방문처리
+    static void exploreRegion(int row, int col, int N, char[][] grid) {
         visited[row][col] = true;
 
         for (int[] dir : directions) {
@@ -23,40 +24,10 @@ public class Main {
             int adjCol = col + dir[1];
 
             if (adjRow < 0 || adjRow >= N || adjCol < 0 || adjCol >= N) continue;
-            if (visited[adjRow][adjCol] || area[row][col] != area[adjRow][adjCol]) continue;
+            // 이미 방문했거나 기존 색깔과 인접 부분의 색이 다르면
+            if (visited[adjRow][adjCol] || grid[row][col] != grid[adjRow][adjCol]) continue;
 
-            findArea(adjRow, adjCol, N, area);
-        }
-    }
-
-
-    static char[][] daltonism(char[][] area, int N) {
-        char[][] daltonism = new char[N][N];
-
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < N; col++) {
-                if (area[row][col] == 'R') {
-                    daltonism[row][col] = 'G';
-
-                } else daltonism[row][col] = area[row][col];
-            }
-        }
-        return daltonism;
-    }
-
-
-
-    static void findDaltonismArea(int row, int col, int N, char[][] dal) {
-        visitedDal[row][col] = true;
-
-        for (int[] dir : directions) {
-            int adjRow = row + dir[0];
-            int adjCol = col + dir[1];
-
-            if (adjRow < 0 || adjRow >= N || adjCol < 0 || adjCol >= N) continue;
-            if (visitedDal[adjRow][adjCol] || dal[row][col] != dal[adjRow][adjCol]) continue;
-
-            findDaltonismArea(adjRow, adjCol, N, dal);
+            exploreRegion(adjRow, adjCol, N, grid);
         }
     }
 
@@ -66,60 +37,64 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
 
-        char[][] area = new char[N][N];
-        visited = new boolean[N][N];
+        grid = new char[N][N];
+        colorBlindGrid = new char[N][N];
 
 
-        visitedDal = new boolean[N][N];
 
+        // 그림 정보 입력받기
         for (int row = 0; row < N; row++) {
             String input = br.readLine();
 
             for (int col = 0; col < N; col++) {
-                area[row][col] = input.charAt(col);
+                grid[row][col] = input.charAt(col);
+                // 입력받을때 적록색약용 그리드도 입력받음
+                colorBlindGrid[row][col] = (grid[row][col] == 'R') ? 'G' : input.charAt(col);
             }
         }
 
-        char[][] dal = daltonism(area, N);
 
-        int areaCount = 0;
+        // 그림을 순회하면서 단지찾기 (비색약인)
+        int regionCount = 0;
+        visited = new boolean[N][N];
 
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < N; col++) {
 
+                // 이미 방문했으면 넘어감
                 if (visited[row][col]) continue;
 
-                areaCount++;
+                regionCount++;
 
-                findArea(row, col, N, area);
+                // 영역처리 함수로 넘기기
+                exploreRegion(row, col, N, grid);
             }
         }
 
 
-        int daltonismCount = 0;
+        // 그림을 순회하면서 단지 찾기 (적록색약)
+        int colorBlindRegionCount = 0;
+        visited = new boolean[N][N];
 
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < N; col++) {
 
-                if (visitedDal[row][col]) continue;
+                // 이미 방문했으면 넘어감
+                if (visited[row][col]) continue;
 
-                daltonismCount++;
+                colorBlindRegionCount++;
 
-                findDaltonismArea(row, col, N, dal);
+                // 영역처리 함수로 넘기기
+                exploreRegion(row, col, N, colorBlindGrid);
             }
         }
 
-
-
-
-        sb.append(areaCount).append(' ');
-        sb.append(daltonismCount);
-
+        sb.append(regionCount).append(' ');
+        sb.append(colorBlindRegionCount);
 
         System.out.println(sb);
-
     }
 }
