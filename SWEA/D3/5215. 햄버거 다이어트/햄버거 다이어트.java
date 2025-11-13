9d9c852cc990301a3043c1f5a2d47b901bd7156e
Giran Oh <ohgiran@gmail.com>
2025-11-13 15:06:09 +0900

    [D3] Title: 햄버거 다이어트, Time: 113 ms, Memory: 28,288 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -5,10 +5,6 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-    static int[][] ingredients;
-    static int N, L;
-    static int max;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -18,44 +14,32 @@ public class Solution {
 
         for (int tc = 1; tc <= T; tc++) {
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            L = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken()); // 재료 수
+            int L = Integer.parseInt(st.nextToken()); // 제한 칼로리
 
-            ingredients = new int[N][2];
+            // 최대 맛 점수 저장
+            int[] dp = new int[L + 1];
 
+            // 재료 갯수만큼 순회 - 모든 재료를 하나씩 순회
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
+                int score = Integer.parseInt(st.nextToken());
+                int cal = Integer.parseInt(st.nextToken());
 
-                ingredients[i][0] = Integer.parseInt(st.nextToken());
-                ingredients[i][1] = Integer.parseInt(st.nextToken());
+                // 배낭 문제 핵심 로직
+                // 역순으로 순회 - 0/1 백팩 문제라면 무조건
+                for (int j = L; j >= cal; j--) {
+
+                    // dp[k]       : 현재 재료를 포함하지 않고, k 칼로리를 채우는 기존의 최대 점수
+                    // dp[k - cal] + score : 현재 재료를 포함시키는 경우의 새로운 점수, 포함시켰으므로 그만큼의 칼로리가 빠짐
+                    dp[j] = Math.max(dp[j], dp[j - cal] + score);
+                }
             }
 
-            max = 0;
-
-            makeBurger(0, 0, 0);
-
-            sb.append("#").append(tc).append(" ").append(max).append("\n");
+            sb.append("#").append(tc).append(" ").append(dp[L]).append("\n");
         }
 
-        System.out.print(sb);
-    }
-
-
-    private static void makeBurger(int index, int scoreSum, int calSum) {
-
-        if (calSum > L) {
-            return;
-        }
-
-        if (index == N) {
-            max = Math.max(max, scoreSum);
-            return;
-        }
-
-        makeBurger(index + 1, scoreSum + ingredients[index][0], calSum + ingredients[index][1]);
-
-
-        makeBurger(index + 1, scoreSum, calSum);
+        System.out.println(sb);
     }
 
 }
