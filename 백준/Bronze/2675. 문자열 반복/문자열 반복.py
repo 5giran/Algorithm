7e5dc0f6f55611ee9e62321831a2b03427ec6992
Giran Oh <ohgiran@gmail.com>
2026-03-08 15:49:06 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -2,16 +2,9 @@ import sys
 input = sys.stdin.readline
 
 t = int(input())
-r_list = list()
 
 for _ in range(t):
-    text = list(input().strip())
-    count = int(text[0])
-    repeat = list()
-    for re in text[2:]:
-        repeat.append(re*count)
+    re_cnt, text = input().split()
+    r_list = [s*int(re_cnt) for s in text]
     
-    r_list.append(''.join(repeat))
-
-for r in r_list:
-    print(r)
+    print(''.join(r_list))
