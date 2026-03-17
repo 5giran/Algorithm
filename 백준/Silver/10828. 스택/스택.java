057305cea2d5458260d80aca4f9b1f53e6696332
Giran Oh <ohgiran@gmail.com>
2026-03-17 20:22:22 +0900

    [Silver IV] Title: 스택, Time: 268 ms, Memory: 20900 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -13,36 +13,30 @@ public class Main {
             if (command.startsWith("push")) {
                 int X = Integer.parseInt(command.split(" ")[1]);
                 stack.add(X);
-            }
 
-            if (command.startsWith("pop")) {
+            } else if (command.equals("pop")) {
                 if (!stack.isEmpty()) {
                     System.out.println(stack.get(stack.size() - 1));
                     stack.remove(stack.size() - 1);
                 } else {
                     System.out.println(-1);
                 }
-            }
 
-            if (command.startsWith("size")) {
+            } else if (command.equals("size")) {
                 System.out.println(stack.size());
-            }
 
-            if (command.startsWith("empty")) {
-                if (stack.isEmpty()) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(0);
-                }
-            }
+            } else if (command.equals("empty")) {
+                System.out.println(stack.isEmpty() ? 1 : 0);
 
-            if (command.startsWith("top")) {
+            } else if (command.equals("top")) {
                 if (!stack.isEmpty()) {
                     System.out.println(stack.get(stack.size() - 1));
                 } else {
                     System.out.println(-1);
                 }
             }
+
+
 
         }
     }
