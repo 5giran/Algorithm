d785660f8939e43a81766e29d2a81ef68b7f8493
Giran Oh <ohgiran@gmail.com>
2026-03-31 19:14:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 1144 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n, k = map(int, input().split())
+    dp = [0] * (k+1)
+    items = []
+
+    for _ in range(n):
+        w, v = map(int, input().split())
+        items.append((w, v))
+
+    for w, v in items:
+        for i in range(k, w-1, -1):
+            dp[i] = max(dp[i], dp[i-w] + v)
+
+    print(dp[k])
+
+if __name__ == "__main__":
+    main()
